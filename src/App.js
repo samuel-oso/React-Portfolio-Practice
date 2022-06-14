99b5db12eeacf801c7b98aee619e090f652a7418
unknown <ososamuel035@gmail.com>
2022-06-14 21:49:32 +0100

    Project Setup/Power button UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./globalStyles";
+import { lightTheme } from "./components/Themes";
+import { Route, Switch } from "react-router";
+
+import Main from "./components/Main";
+import AboutPage from "./components/AboutPage";
+import BlogPage from "./components/BlogPage";
+import WorkPage from "./components/WorkPage";
+import MySkillsPage from "./components/MySkillsPage";
+
 function App() {
-  return <div>
-    App File
+  return (
+    <div>
+      <GlobalStyle />
+
+      <ThemeProvider theme={lightTheme}>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/blog" component={BlogPage} />
+          <Route exact path="/work" component={WorkPage} />
+          <Route exact path="/skills" component={MySkillsPage} />
+        </Switch>
+      </ThemeProvider>
     </div>
-    
+  );
 }
 
-export default App
-
+export default App;
---
+++ src/components/MySkillsPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MySkillsPage() {
+  return <div>MySkillsPage</div>;
+}
+
+export default MySkillsPage;
