4309714c3843efc25314481bb0c15bc95c31e976
unknown <ososamuel035@gmail.com>
2022-06-15 22:24:06 +0100

    Blog Page

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import img from "../assets/Images/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg";
+import SocialIcons from "../SubComponents/SocialIcons";
+import LogoComponent from "../SubComponents/LogoComponent";
+import PowerButton from "../SubComponents/PowerButton";
+import { Blogs } from "../data/BlogData";
+import BlogComponent from "./BlogComponent";
+import AnchorComponent from "../SubComponents/Anchor";
 
-function BlogPage() {
-  return <div>BlogPage</div>;
-}
+const MainContainer = styled.div`
+  background-image: url(${img});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+`;
+
+const Container = styled.div`
+  background-color: ${(props) => `rgba(${props.theme.bodyRgba},0.8)`};
+  width: 100%;
+  height: auto;
+  positon: relative;
+  padding-bottom: 5rem;
+`;
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10rem;
+`;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
+  grid-gap: calc(1rem + 2vw);
+`;
+
+const BlogPage = () => {
+  const [numbers, setNumbers] = useState(0);
+
+  useEffect(() => {
+    let num = (window.innerHeight - 70) / 30;
+    setNumbers(parseInt(num));
+  }, []);
+
+  return (
+    <MainContainer>
+      <Container>
+        <LogoComponent />
+        <PowerButton />
+        <SocialIcons />
+        <AnchorComponent numbers={numbers} />
+
+        <Center>
+          <Grid>
+            {Blogs.map((blog) => {
+              return <BlogComponent key={blog.id} blog={blog} />;
+            })}
+          </Grid>
+        </Center>
+      </Container>
+    </MainContainer>
+  );
+};
 
 export default BlogPage;
